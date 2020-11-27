3ade0778b5023c091ac18cf710b72b9967841812
JUNGSUNWOO <tjsdn970924@naver.com>
2020-11-27 13:28:24 +0900

    4pg 틀

--- frontend/project/app/src/main/java/com/example/myapplication/HomePage.java
+++ frontend/project/app/src/main/java/com/example/myapplication/HomePage.java
@@ -13,36 +13,32 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-<<<<<<< HEAD
         Button btn_me;
         Button btn_fri;
-        btn_me = (Button)findViewById(R.id.btn_me);
-        btn_me.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        Button btn_myinfo;
+        btn_me = (Button) findViewById(R.id.btn_me);
+        btn_me.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent = new Intent(HomePage.this, page_4.class);
                 startActivity(intent);
             }
         });
-        btn_fri = (Button)findViewById(R.id.btn_fri);
-        btn_fri.setOnClickListener(new View.OnClickListener(){
-=======
-        Button button;
-        Button button3;
-        button3 = (Button)findViewById(R.id.button3);
-        button = (Button)findViewById(R.id.button);
-        button3.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(HomePage.this,myinformation.class);
-                startActivity(intent);
-            }
-
-        });
-        button.setOnClickListener(new View.OnClickListener(){
->>>>>>> b5f106e2d58a58c0fc6b38b584cc973523f21925
-            public void onClick(View v){
+        btn_fri = (Button) findViewById(R.id.btn_fri);
+        btn_fri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent = new Intent(HomePage.this, MainActivity2.class);
                 startActivity(intent);
+            }
+        });
+        btn_myinfo = (Button) findViewById(R.id.btn_myinfo);
+        btn_myinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, myinformation.class);
+                startActivity(intent);
+
             }
         });
     }
